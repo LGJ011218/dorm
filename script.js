e76930a7edf5360d5b8338fb0e0a00fcc14a5d1d
LGJ011218 <lkj011218@naver.com>
2025-02-27 01:10:10 +0900

    '250227'

--- script.js
+++ script.js
@@ -29,31 +29,57 @@ function calculateDaysFrom2001() {
       "흑상관", "흑상관"
     ];
   
-    // 결과값 만들기
-    let resultText = "";
+    let dormitory = "";
     if (daysDifference >= 0) {
-      resultText = `${name}님의 기숙사는 ${sajuList[numForFinding]}입니다.`;
+      dormitory = sajuList[numForFinding];
     } else {
-      // daysDifference < 0일 때 배열 인덱스를 위해 '10 + numForFinding'
-      resultText = `${name}님의 기숙사는 ${sajuList[10 + numForFinding]}입니다.`;
+      dormitory = sajuList[10 + numForFinding];
     }
+  
+    let resultText = `${name}님의 기숙사는 ${dormitory}입니다.`;
   
     // 결과 표시
     document.getElementById("result").innerText = resultText;
   
+    // 결과 이미지 설정
+    let imageElement = document.getElementById("resultImage");
+    // 기숙사 이름에 따른 이미지 경로 지정 (이미지는 미리 준비되어야 합니다)
+    switch (dormitory) {
+      case "청양관":
+        imageElement.src = "images/blue.png";
+        break;
+      case "적초관":
+        imageElement.src = "images/red.png";
+        break;
+      case "황접관":
+        imageElement.src = "images/yellow.png";
+        break;
+      case "백작관":
+        imageElement.src = "images/white.png";
+        break;
+      case "흑상관":
+        imageElement.src = "images/black.png";
+        break;
+      default:
+        imageElement.src = "";
+    }
+    // 이미지 표시
+    imageElement.style.display = "block";
+  
     // 입력 창 숨기고 결과 창 보이기
     document.getElementById("inputContainer").style.display = "none";
     document.getElementById("resultContainer").style.display = "flex";
-  }
+}
   
-
 function resetForm() {
     // 입력값 초기화
     document.getElementById("name").value = "";
     document.getElementById("birthdate").value = "";
   
+    // 결과 이미지 숨기기
+    document.getElementById("resultImage").style.display = "none";
+  
     // 결과 화면 숨기고 입력 화면 다시 보이기
     document.getElementById("resultContainer").style.display = "none";
     document.getElementById("inputContainer").style.display = "flex";
-  }
-  
+}
